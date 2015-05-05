81e749454346a3c9ccd12bee6f30400759823be3
Jorge H. P. Garcia <j.hrique@hotmail.com>
2015-05-05 10:40:00 -0300

    Paddle e seu movimento adicionado

--- SpriteKit_tutorial_NotSoEasy/GameScene.swift
+++ SpriteKit_tutorial_NotSoEasy/GameScene.swift
@@ -13,14 +13,26 @@ let PaddleCategoryName = "paddle"
 let BlockCategoryName = "block"
 let BlockNodeCategoryName = "blockNode"
 
+var isFingerOnPaddle = false
+
+
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         super.didMoveToView(view)
         
         physicsWorld.gravity = CGVectorMake(0, 0)
+
         
         let ball = childNodeWithName(BallCategoryName) as! SKSpriteNode
         ball.physicsBody!.applyImpulse(CGVectorMake(10, -10))
+        
+        // Já definido no .sks (só para lembrar dps por código)
+        ball.physicsBody!.allowsRotation = false
+        ball.physicsBody!.friction = 0
+        ball.physicsBody!.restitution = 1
+        ball.physicsBody!.linearDamping = 0
+        ball.physicsBody!.angularDamping = 0
+
         
         // 1. Create a physics body that borders the screen
         let borderBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
@@ -32,4 +44,61 @@ class GameScene: SKScene {
         
         
     }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        var touch = touches.first as! UITouch
+        var touchLocation = touch.locationInNode(self)
+        
+        if let body = physicsWorld.bodyAtPoint(touchLocation) {
+            if body.node!.name == PaddleCategoryName {
+                println("Began touch on paddle")
+                isFingerOnPaddle = true
+            }
+        }
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        isFingerOnPaddle = false
+    }
+
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        // 1. Check whether user touched the paddle
+        if isFingerOnPaddle {
+            // 2. Get touch location
+            var touch = touches.first as! UITouch
+            var touchLocation = touch.locationInNode(self)
+            var previousLocation = touch.previousLocationInNode(self)
+            
+            // 3. Get node for paddle
+            var paddle = childNodeWithName(PaddleCategoryName) as! SKSpriteNode
+            
+            // 4. Calculate new position along x for paddle
+            var paddleX = paddle.position.x + (touchLocation.x - previousLocation.x)
+            
+            // 5. Limit x so that paddle won't leave screen to left or right
+            paddleX = max(paddleX, paddle.size.width/2)
+            paddleX = min(paddleX, size.width - paddle.size.width/2)
+            
+            // 6. Update paddle position
+            paddle.position = CGPointMake(paddleX, paddle.position.y)
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
