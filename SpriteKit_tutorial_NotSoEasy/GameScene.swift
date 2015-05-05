999e5c07479547989187375ee59e273c93388026
Jorge H. P. Garcia <j.hrique@hotmail.com>
2015-05-05 10:05:05 -0300

    edge-based body added to borders

--- SpriteKit_tutorial_NotSoEasy/GameScene.swift
+++ SpriteKit_tutorial_NotSoEasy/GameScene.swift
@@ -16,5 +16,15 @@ let BlockNodeCategoryName = "blockNode"
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         super.didMoveToView(view)
+        
+        // 1. Create a physics body that borders the screen
+        let borderBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        // 2. Set the friction of that physicsBody to 0
+        borderBody.friction = 0
+        // 3. Set physicsBody of scene to borderBody
+        self.physicsBody = borderBody
+        
+        
+        
     }
 }
